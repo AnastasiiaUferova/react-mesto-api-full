268d370cc35a92490ae6ab9cdda8f0cef7801f94
‘Anastasiia <miss.iuferova@yandex.ru>
2022-05-11 12:47:55 +0200

    corrected backend

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,8 +4,7 @@ const allowedCors = [
 ];
 
 module.exports.cors = (req, res, next) => {
-    const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-    // проверяем, что источник запроса есть среди разрешённых 
+    const { origin } = req.headers; 
     if (allowedCors.includes(origin)); {
         res.header('Access-Control-Allow-Origin', origin);
         res.header('Access-Control-Allow-Credentials', true)
