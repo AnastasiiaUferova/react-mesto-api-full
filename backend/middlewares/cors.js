f5e39dffb6d407e962651e4dce122949eae88db8
‘Anastasiia <miss.iuferova@yandex.ru>
2022-05-11 12:40:07 +0200

    corrected backend

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,14 @@
 const allowedCors = [
     'localhost:3000',
-    'https://mesto-front.u.nomoredomains.xyz/'
+    'htpps:localhost:3000',
+    'https://mesto-front.u.nomoredomains.xyz/signup',
+    'https://mesto-front.u.nomoredomains.xyz/sigin',
+    'http://mesto-front.u.nomoredomains.xyz/signup',
+    'http://mesto-front.u.nomoredomains.xyz/signin',
+    'http://mesto-back.u.nomoredomains.xyz/signup',
+    'https://mesto-back.u.nomoredomains.xyz/signin',
+    'https://mesto-front.u.nomoredomains.xyz/signup'
+
 ];
 
 module.exports.cors = (req, res, next) => {
@@ -8,7 +16,17 @@ module.exports.cors = (req, res, next) => {
     // проверяем, что источник запроса есть среди разрешённых 
     if (allowedCors.includes(origin)); {
         res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Credentials', true)
     }
+
+    const { method } = req; 
+    const requestHeaders = req.headers['access-control-request-headers'];
+    const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; 
+    if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+} 
     next();
 }; 
 
