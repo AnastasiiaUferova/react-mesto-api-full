05a5daf289d2288df75c31d9f4acf6502dbdb316
‘Anastasiia <miss.iuferova@yandex.ru>
2022-05-15 12:00:35 +0200

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
+const cors = require('./middlewares/cors')
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
@@ -14,6 +15,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
