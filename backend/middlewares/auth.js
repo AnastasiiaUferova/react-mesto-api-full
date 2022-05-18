be5efc530ea1cf6ce1479cfc0f2a82d747dbcf42
‘Anastasiia <miss.iuferova@yandex.ru>
2022-05-18 15:27:26 +0200

    corrected frontend e-mail

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
     // попытаемся верифицировать токен
     payload = jwt.verify(token, 'super-secret-strong-web-code');
   } catch (err) {
-    console.log(jwt)
+    console.log(err)
     throw new UnauthorizedError('Необходима авторизация2');
   }
 
